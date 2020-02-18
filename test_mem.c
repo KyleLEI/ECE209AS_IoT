5ad622e222690b0abee1bdc36dbd2e43f31d76b8
KyleLEI <leizhao2@gmail.com>
2020-02-18 14:05:47 -0800

    switched to loop id

--- test_mem.c
+++ test_mem.c
@@ -1,21 +1,23 @@
-#include <stddef.h>
+#include <unistd.h>
 
-#define ACCESS_COUNT 10000000
-typedef char data_t;
+#define LOOP_COUNT 10000000
+typedef int data_t;
 
 data_t a = 0;
 data_t b = 1;
 
 int main(int argc, char* argv[]){
-	data_t t;
-	register data_t* ptr = &a;
-
-	/* Load 0s */
-    for(register size_t i=0;i<ACCESS_COUNT;++i)
-        t = *ptr;
+	data_t x = a;
 	
-	/* Load 1s */
-	ptr = &b;
-	for(register size_t i=0;i<ACCESS_COUNT;++i)
-		t = *ptr;
+	if(x==a)
+    	for(register size_t i=0;i<LOOP_COUNT;++i){
+        	x = 50*50; // arithmetic operation
+			usleep(10);
+		}
+	else	
+		for(register size_t i=0;i<LOOP_COUNT;++i){
+			x = b; // read from memory
+			usleep(10);
+		}
+	return 0;
 }
